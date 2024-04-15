3619d6b1662ae63a7b34a18f830f17c44e7b8302
Ezhil-ezy <ezhil2pointo@gmail.com>
2024-04-15 18:57:52 +0530

    searching the target

--- problemArray.py
+++ problemArray.py
@@ -1,6 +1,7 @@
 class array:
 
   array = [26, 22, 13, 25, 29, 5, 6, 1, 17, 9, 11, 23, 16 , 15]
+  target = int(input('enter your target value:\t'))
 
   def largestElement(self):
     print('the largest element is : ', max(self.array))
@@ -26,10 +27,24 @@ class array:
 
     print('the smallest element is : ', minimum)  
 
+  def searchTarget(self):
+
+    k = self.target
+    array = self.array
+
+    if k in array:
+      for idx, value in enumerate(array):
+        if k == value:
+          print(f'the element present in the place of :', idx)
+
+    else: print(f'the element {k} which is not present in an array')
+    
 
 if __name__ == '__main__':
+
   Obj = array()
 
+  Obj.searchTarget()
   Obj.largestElement()
   Obj.smallestElement()
   Obj.largestElementAlgo()
