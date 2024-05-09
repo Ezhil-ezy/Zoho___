bb31e095e3a148a49077c7747a47cc0ba62a1ad9
Ezhil-ezy <ezhil2pointo@gmail.com>
2024-05-09 22:25:59 +0530

    linearsearch

--- searchingAlgo.py
+++ searchingAlgo.py
@@ -1,12 +1,34 @@
 # searching algorithm _________________
 
 class searchingAlgorithm:
-  def searching(self, array, n, k):
-    pass
+  
+  # Linear Search: Sequentially checks each element in the collection 
+  # until the target element is found or the end of the collection is reached.
+  def linearSearch(self, array, n, k):
+
+    # using for loop
+    if k in array:
+      for idx in range(n):
+        if array[idx] == k:
+          return f'Target element {k} is presented in position of {idx + 1}'
+    else:
+      return f'Target element {k} is not found'
+    
+    # using while loop
+    if k in array:
+      idx = 0 
+      while idx < n:
+        if array[idx] == k:
+          return f'Target element {k} is presented in position of {idx + 1}'
+        idx += 1
+    else:
+      return f'Target element {k} is not found'
+    
+
 if __name__ == '__main__' :
-  n = int(input('Enter the length of the array:\t'))
-  k = int(input('Enter the target element of the array:\t'))
+  number = int(input('Enter the length of the array:\t'))
+  target = int(input('Enter the target element of the array :\t'))
   array = list(map(int, input().strip().split()))
-  obj = searchingAlgorithm().searching(array, n, k)
+  obj = searchingAlgorithm().linearSearch(array, number, target)
   print(obj)
   
